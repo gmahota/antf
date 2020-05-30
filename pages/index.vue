4ff3891192b701fb053990986ce76d785b63c81b
Guimarães Mahota <guimaraesmahota@gmail.com>
2020-05-30 21:45:49 +0200

    add page 1 and 2

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,14 @@
   <div>
     <hero />
     <theme-features />
+    <features />,
+    <contact-us />',
+    <affiliates />,
+    <social-media />,
+    <keep-in-touch />,
+    <newsletter />,
+    <pro-features />,
+    <info />,
   </div>
 </template>
 
@@ -10,7 +18,15 @@ export default {
   layout: "home",
   components: {
     Hero: () => import("@/components/sections/Hero"),
-    ThemeFeatures: () => import("@/components/sections/ThemeFeatures")
+    ThemeFeatures: () => import("@/components/sections/ThemeFeatures"),
+    ContactUs: () => import("@/components/sections/ContactUs"),
+    Affiliates: () => import("@/components/sections/Affiliates"),
+    SocialMedia: () => import("@/components/sections/SocialMedia"),
+    
+    KeepInTouch: () => import("@/components/sections/KeepInTouch"),
+    Newsletter: () => import("@/components/sections/Newsletter"),
+    ProFeatures: () => import("@/components/sections/ProFeatures"),
+    Info: () => import("@/components/sections/info")
   }
 };
 </script>
